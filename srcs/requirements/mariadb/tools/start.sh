02a421d3fd241947b5baef9e066cfa776ff0abe6
alones-dev <kyliandaumont78@gmail.com>
2024-09-16 04:55:09 -0400

    bug mysql

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+mysqld_safe;
+sleep 5
 
-echo "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};" > /etc/mysql/init.sql
-echo "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" >> /etc/mysql/init.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO '${SQL_USER}'@'%' WITH GRANT OPTION;" >> /etc/mysql/init.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
+mysql -e "CREATE DATABASE IF NOT EXISTS `${SQL_DATABASE}`;"
 
-mysqld
+mysql -e "CREATE USER IF NOT EXISTS `${SQL_USER}`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "GRANT ALL PRIVILEGES ON `${SQL_DATABASE}`.* TO `${SQL_USER}`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+exec mysqld_safe
